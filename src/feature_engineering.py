e5cb2d9ca7476103cd73d7d3470dd5466dd64e68
Ashutosh03u <64391669+Ashutosh03u@users.noreply.github.com>
2026-04-23 17:14:26 +0200

    Update feature_engineering.py

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,1 +1,15 @@
+# =========================================================
+# FILE: feature_engineering.py
+# =========================================================
 
+import pandas as pd
+
+df = pd.read_csv("activity_data.csv")
+
+df["activity_change"] = df["activity_score"].pct_change()
+
+df["activity_change"] = df["activity_change"].fillna(0)
+
+df.to_csv("featured_data.csv", index=False)
+
+print(df.head())
